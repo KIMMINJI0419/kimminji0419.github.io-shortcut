e14997ff9aaffccdabc052823087fe75c15a4813
KIMMINJI0419 <63002440+KIMMINJI0419@users.noreply.github.com>
2020-11-16 18:45:09 +0900

    d
    d

--- js/hash_slide.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    var slideIndex = 0;
-    var $btn = $('.hash_btn').on('click', function() {
-        var idx = $btn.index(this);
-        $(".banner-container").animate({marginLeft:-265 * idx});
-        slideIndex = $btn.index(); 
-      });
-
- });
--- js/mobile_menu.js
+++
@@ -1,6 +0,0 @@
-
-var nav = document.querySelector("div.navigation");
-function ClickbtnNav(e){
-    nav.classList.toggle("active");
-}
-nav.addEventListener("click", ClickbtnNav);
